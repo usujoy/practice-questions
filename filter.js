99492351a923a13b68123e44a6e2f2217d14114e
usujoy <183721610+usujoy@users.noreply.github.com>
2024-12-22 22:27:24 +0530

    added function for filter long words

--- filter.js
+++ filter.js
@@ -7,7 +7,15 @@ const filterEvenNumbers = function (numbers) {
 };
 
 // words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
-const filterLongWords = function (words) { };
+const isGreaterThan = function (length) {
+  return function (word) {
+    return word.length > length;
+  }
+};
+
+const filterLongWords = function (words) {
+  return words.filter(isGreaterThan(5))
+};
 
 // people older than 30 [{name: "Alice", age: 25}, {name: "Bob", age: 35}] => [{name: "Bob", age: 35}]
 const filterAdults = function (people) { };
